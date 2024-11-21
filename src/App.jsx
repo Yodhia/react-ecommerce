1c8c42faa300c09d828a8bc0eef8554493086bbc
Yodhia Ang <yodhia@bells.sg>
2024-11-21 16:26:00 +0000

    product card

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from "./Header"
 import "./styles.css"
+import ProductCard from "./ProductCard"
 
 
 export default function App(){
@@ -42,61 +43,32 @@ export default function App(){
     <main className="container my-5">
       <h2 className="text-center mb-4">Featured Products</h2>
       <div className="row">
+       <ProductCard
+        imageUrl="https://picsum.photos/id/1/300/200"
+        productName="Anvil"
+        price="$100"
+       />
+
         <div className="col-md-3 mb-4">
-          <div className="card">
-            <img
-              src="https://picsum.photos/id/20/300/200"
-              className="card-img-top"
-              alt="Product 1"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Product 1</h5>
-              <p className="card-text">$19.99</p>
-              <a href="#" className="btn btn-primary">Add to Cart</a>
-            </div>
-          </div>
+          <ProductCard
+            imageUrl="https://picsum.photos/id/1/300/200"
+            productName="Tool Bag"
+            price="$50"
+          />
         </div>
         <div className="col-md-3 mb-4">
-          <div className="card">
-            <img
-              src="https://picsum.photos/id/1/300/200"
-              className="card-img-top"
-              alt="Product 2"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Product 2</h5>
-              <p className="card-text">$29.99</p>
-              <a href="#" className="btn btn-primary">Add to Cart</a>
-            </div>
-          </div>
+          <ProductCard
+            imageUrl="https://picsum.photos/id/26/300/200"
+            productName="Hammer"
+            price="$45"
+          />
         </div>
         <div className="col-md-3 mb-4">
-          <div className="card">
-            <img
-              src="https://picsum.photos/id/26/300/200"
-              className="card-img-top"
-              alt="Product 3"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Product 3</h5>
-              <p className="card-text">$39.99</p>
-              <a href="#" className="btn btn-primary">Add to Cart</a>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-3 mb-4">
-          <div className="card">
-            <img
-              src="https://picsum.photos/id/96/300/200"
-              className="card-img-top"
-              alt="Product 4"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Product 4</h5>
-              <p className="card-text">$49.99</p>
-              <a href="#" className="btn btn-primary">Add to Cart</a>
-            </div>
-          </div>
+          <ProductCard
+            imageUrl="https://picsum.photos/id/96/300/200"
+            productName="Screwdriver"
+            price="$13"
+          />
         </div>
       </div>
     </main>
